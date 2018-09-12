308a47d4c4269764711c51fbcf485b0c140eb0d8
sunhao <849142551@qq.com>
2018-09-12 14:19:37 +0800

    jt-eureka-server

---
+++ jt-eureka-server/src/main/java/cn/jt/EurakaServer.java
@@ -0,0 +1,13 @@
+package cn.jt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurakaServer {
+	public static void main(String[] args) {
+		SpringApplication.run(EurakaServer.class, args);
+	}
+}
